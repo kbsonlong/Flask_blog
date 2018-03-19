c0af18e122377aa2bbe9dc3507321919c72fb4c9
kbsonlong <kbsonlong@gmail.com>
2018-03-19 15:28:30 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+class Config(object):
+    """Base config class."""
+    pass
+
+class ProdConfig(Config):
+    """Production config class."""
+    pass
+
+class DevConfig(Config):
+    """Development config class."""
+    # Open the DEBUG
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://kbsonlong:kbsonlong@127.0.0.1:3306/kbson'
